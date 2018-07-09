a7cb728d2470b0c08922dadf54626b8accf66af4
Merlin Beutlberger <m.beutlberger@sap.com>
2018-07-09 16:36:37 +0200

    [FEATURE] Add update notifier
    Asynchronously checks for newer ui5-cli releases and notifies user on
    next run. Also see: https://github.com/yeoman/update-notifier#how

--- bin/ui5.js
+++ bin/ui5.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
+const updateNotifier = require("update-notifier");
 const cli = require("yargs");
+const pkg = require("../package.json");
+
+updateNotifier({
+	pkg,
+	updateCheckInterval: 1000 * 60 * 60 * 24, // 1 day
+	shouldNotifyInNpmScript: true
+}).notify();
 
 // CLI modules
 cli.commandDir("../lib/cli/commands");
