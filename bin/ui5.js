80b5bfe1d53494889c794171109321ebbd3f8d61
Merlin Beutlberger <m.beutlberger@sap.com>
2018-07-10 17:30:08 +0200

    [FEATURE] Add Node.js version check
    Lower code coverage statements threshold until we have proper CLI tests

--- bin/ui5.js
+++ bin/ui5.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
+
+// The following block should be compatible to as many Node.js versions as possible
+/* eslint-disable no-var */
+var pkg = require("../package.json");
+var semver = require("semver");
+var nodeVersion = process.version;
+/* eslint-enable no-var */
+if (pkg.engines && pkg.engines.node && !semver.satisfies(nodeVersion, pkg.engines.node)) {
+	console.log("==================== UNSUPPORTED NODE.JS VERSION ====================");
+	console.log("You are using an unsupported version of Node.js");
+	console.log("Detected version " + nodeVersion + " but " + pkg.name + " requires " + pkg.engines.node);
+	console.log("");
+	console.log("=> Please upgrade to a supported version of Node.js to use this tool");
+	console.log("=====================================================================");
+	process.exit(1);
+}
+
 const updateNotifier = require("update-notifier");
 const cli = require("yargs");
-const pkg = require("../package.json");
 
 updateNotifier({
 	pkg,
