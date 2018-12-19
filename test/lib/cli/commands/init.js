93db6fe1021d6b59b3c2ae34135fa30babb92aa0
Roman Rommel <roman.rommel@sap.com>
2018-12-19 12:23:51 +0100

    [INTERNAL] increased code coverage and refactored some modules

--- test/lib/cli/commands/init.js
+++ test/lib/cli/commands/init.js
@@ -1,37 +1,39 @@
 const {test} = require("ava");
 const sinon = require("sinon");
+const validate = require("../../../../lib/utils/validate");
 const initCommand = require("../../../../lib/cli/commands/init");
 const path = require("path");
-// const init = require("../../../../lib/init/init");
-// const jsYaml = require("js-yaml");
-// const fs = require("fs");
+const init = require("../../../../lib/init/init");
+const jsYaml = require("js-yaml");
+const fs = require("fs");
 
-test.serial("ui5.yaml exists", async (t) => {
-	const bExists = await initCommand.fileExists(
-		path.join("test", "fixtures", "init", "application", "ui5.yaml")
-	);
-	t.is(bExists, true, "ui5.yaml was found");
+test.serial("Writes ui5.yaml to fs", async (t) => {
+	const ui5YamlPath = "./ui5.yaml";
+	const ui5Yaml = `
+	specVersion: '0.1'
+	metadata:
+	name: sample-app
+	type: application`;
+	sinon.stub(path, "resolve").returns(ui5YamlPath);
+	sinon.stub(validate, "exists").resolves(false);
+	const initStub = sinon.stub(init, "init").resolves({});
+	const safeDumpYamlStub = sinon.stub(jsYaml, "safeDump").returns(ui5Yaml);
+	const fsWriteFileStub = sinon.stub(fs, "writeFile").callsArgWith(2);
+
+	await initCommand.handler();
+
+	t.is(fsWriteFileStub.getCall(0).args[0], ui5YamlPath, "Passes yaml path to write the yaml file to");
+	t.is(fsWriteFileStub.getCall(0).args[1], ui5Yaml, "Passes yaml content to write to fs");
+
+	validate.exists.restore();
+	path.resolve.restore();
+	initStub.restore();
+	safeDumpYamlStub.restore();
+	fsWriteFileStub.restore();
 });
 
-// test.serial("Writes ui5.yaml to fs", async (t) => {
-// 	const pathResolveStub = sinon.stub(path, "resolve").resolves("project/ui5.yaml");
-// 	const fileExistsStub = sinon.stub(initCommand, "fileExists").resolves(false);
-// 	const initStub = sinon.stub(init, "init").resolves(true);
-// 	const safeDumpYamlStub = sinon.stub(jsYaml, "safeDump").resolves("specVersion: 0");
-// 	const fsWriteFileStub = sinon.stub(fs, "writeFile").resolves(true);
-
-// 	await initCommand.handler();
-// 	t.is(fsWriteFileStub.getCall(0).args, "bla", "Writes ui5.yaml to fs");
-
-// 	fileExistsStub.restore();
-// 	pathResolveStub.restore();
-// 	initStub.restore();
-// 	safeDumpYamlStub.restore();
-// 	fsWriteFileStub.restore();
-// });
-
 test.serial("Error: throws if ui5.yaml already exists", async (t) => {
-	const fileExistsStub = sinon.stub(initCommand, "fileExists").resolves(true);
+	const fileExistsStub = sinon.stub(validate, "exists").resolves(true);
 	const processExitStub = sinon.stub(process, "exit");
 
 	await initCommand.handler();
