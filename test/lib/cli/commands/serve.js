57d5567b2e78a6d5ce8bbc6cae50b5d7bec80da4
Frank Weigel <frank.weigel@sap.com>
2019-04-26 10:18:53 +0200

    [FEATURE] Add 'sap-csp-policies' option to 'serve' command. (#188)
    CLI counterpart for https://github.com/SAP/ui5-server/pull/179

--- test/lib/cli/commands/serve.js
+++ test/lib/cli/commands/serve.js
@@ -56,7 +56,8 @@ test.serial("ui5 serve: default", async (t) => {
 		h2: false,
 		port: 8080,
 		cert: undefined,
-		key: undefined
+		key: undefined,
+		sendSAPTargetCSP: false
 	}, "Starting server with specific server config");
 });
 
@@ -91,7 +92,8 @@ test.serial("ui5 serve --h2", async (t) => {
 		h2: true,
 		port: 8443,
 		key: "randombyte-likes-ponies-key",
-		cert: "randombyte-likes-ponies-cert"
+		cert: "randombyte-likes-ponies-cert",
+		sendSAPTargetCSP: false
 	}, "Starting server with specific server config");
 });
 
@@ -161,7 +163,8 @@ test.serial("ui5 serve --key --cert", async (t) => {
 		h2: true,
 		port: 8443,
 		key: "ponies-loaded-from-custompath-key",
-		cert: "ponies-loaded-from-custompath-crt"
+		cert: "ponies-loaded-from-custompath-crt",
+		sendSAPTargetCSP: false
 	}, "Starting server with specific server config");
 });
 
@@ -181,4 +184,30 @@ test.serial("ui5 serve --translator --config", async (t) => {
 		translatorName: "static",
 		configPath: "path/to/my/config.json"
 	}, "CLI was called with static translator");
+});
+
+test.serial("ui5 serve --sap-csp-policies", async (t) => {
+	normalizerStub.resolves(projectTree);
+	serverStub.resolves({});
+
+	// loads project tree using http 2
+	const pPrepareServerConfig = await serve.handler(Object.assign({}, defaultInitialHandlerArgs, {sapCspPolicies: true}));
+	// preprocess project config
+	const pServeServer = await pPrepareServerConfig;
+	// serve server using config
+	await pServeServer;
+
+	const injectedProjectTree = serverStub.getCall(0).args[0];
+	const injectedServerConfig = serverStub.getCall(0).args[1];
+
+	t.deepEqual(injectedProjectTree, projectTree, "Starting server with given project tree");
+	t.deepEqual(injectedServerConfig, {
+		changePortIfInUse: true,
+		acceptRemoteConnections: false,
+		h2: false,
+		port: 8080,
+		cert: undefined,
+		key: undefined,
+		sendSAPTargetCSP: true
+	}, "Starting server with specific server config");
 });
