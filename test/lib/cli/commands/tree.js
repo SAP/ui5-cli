SAP/ui5-cli

commit c7c607a6f9337ec80f99b8650bef05fd495a6a71

[INTERNAL] Make all command handlers async and propagate errors to base middleware

lib/cli/commands/tree.js
@@ -26,11 +26,10 @@ tree.builder = function(cli) {
 		.example("ui5 tree --json > tree.json", "Pipes the dependency tree into a new file \"tree.json\"");
 };
 
-tree.handler = function(argv) {
+tree.handler = async function(argv) {
 	const normalizer = require("@ui5/project").normalizer;
 	const treeify = require("treeify");
 
-	let p;
 	const options = {
 		translatorName: argv.translator,
 		translatorOptions: {
@@ -39,19 +38,13 @@ tree.handler = function(argv) {
 		configPath: argv.config
 	};
 	if (argv.full) {
-		p = normalizer.generateProjectTree(options);
+		await normalizer.generateProjectTree(options);
 	} else {
-		p = normalizer.generateDependencyTree(options);
+		await normalizer.generateDependencyTree(options);
 	}
 
-	p.then(function(tree) {
-		const output = argv.json ? JSON.stringify(tree, null, 4) : treeify.asTree(tree, true);
-		console.log(output);
-		return output;
-	}).catch(function(err) {
-		console.error(err);
-		process.exit(1);
-	});
+	const output = argv.json ? JSON.stringify(tree, null, 4) : treeify.asTree(tree, true);
+	console.log(output);
 };
 
 module.exports = tree;

lib/cli/commands/build.js
@@ -70,7 +70,7 @@ build.builder = function(cli) {
 			"Build project and dependencies in dev mode. Only a set of essential tasks is executed.");
 };
 
-function handleBuild(argv) {
+async function handleBuild(argv) {
 	const normalizer = require("@ui5/project").normalizer;
 	const builder = require("@ui5/builder").builder;
 	const logger = require("@ui5/logger");
@@ -78,22 +78,21 @@ function handleBuild(argv) {
 	const command = argv._[argv._.length - 1];
 	logger.setShowProgress(true);
 
-	return normalizer.generateProjectTree({
+	const tree = await normalizer.generateProjectTree({
 		translatorName: argv.translator,
 		configPath: argv.config
-	}).then(function(tree) {
-		return builder.build({
-			tree: tree,
-			destPath: argv.dest,
-			cleanDest: argv["clean-dest"],
-			buildDependencies: argv.all,
-			dev: command === "dev",
-			selfContained: command === "self-contained",
-			jsdoc: command === "jsdoc",
-			devExcludeProject: argv["dev-exclude-project"],
-			includedTasks: argv["include-task"],
-			excludedTasks: argv["exclude-task"]
-		});
+	});
+	await builder.build({
+		tree: tree,
+		destPath: argv.dest,
+		cleanDest: argv["clean-dest"],
+		buildDependencies: argv.all,
+		dev: command === "dev",
+		selfContained: command === "self-contained",
+		jsdoc: command === "jsdoc",
+		devExcludeProject: argv["dev-exclude-project"],
+		includedTasks: argv["include-task"],
+		excludedTasks: argv["exclude-task"]
 	});
 }
 

lib/cli/commands/init.js
@@ -14,22 +14,17 @@ initCommand.handler = async function() {
 	const jsYaml = require("js-yaml");
 	const writeFile = promisify(fs.writeFile);
 
-	try {
-		const yamlPath = path.resolve("./ui5.yaml");
-		if (await fsHelper.exists(yamlPath)) {
-			throw new Error("Initialization not possible: ui5.yaml already exists");
-		}
-
-		const projectConfig = await init.init();
-		const yaml = jsYaml.safeDump(projectConfig);
-
-		await writeFile(yamlPath, yaml);
-		console.log(`Wrote ui5.yaml to ${yamlPath}:\n`);
-		console.log(yaml);
-	} catch (err) {
-		console.error(err);
-		process.exit(1);
+	const yamlPath = path.resolve("./ui5.yaml");
+	if (await fsHelper.exists(yamlPath)) {
+		throw new Error("Initialization not possible: ui5.yaml already exists");
 	}
+
+	const projectConfig = await init.init();
+	const yaml = jsYaml.safeDump(projectConfig);
+
+	await writeFile(yamlPath, yaml);
+	console.log(`Wrote ui5.yaml to ${yamlPath}:\n`);
+	console.log(yaml);
 };
 
 module.exports = initCommand;

lib/cli/commands/serve.js
@@ -54,79 +54,69 @@ serve.builder = function(cli) {
 			"Listen to port 1337 and launch default browser with http://localhost:1337/test/QUnit.html");
 };
 
-serve.handler = function(argv) {
+serve.handler = async function(argv) {
 	const normalizer = require("@ui5/project").normalizer;
 	const ui5Server = require("@ui5/server");
 	const server = ui5Server.server;
 
-	normalizer.generateProjectTree({
+	const tree = await normalizer.generateProjectTree({
 		translatorName: argv.translator,
 		configPath: argv.config
-	}).then(function(tree) {
-		let port = argv.port;
-		let changePortIfInUse = false;
-
-		if (!port && tree.server && tree.server.settings) {
-			if (argv.h2) {
-				port = tree.server.settings.httpsPort;
-			} else {
-				port = tree.server.settings.httpPort;
-			}
-		}
-
-		if (!port) {
-			changePortIfInUse = true; // only change if port isn't explicitly set
-			if (argv.h2) {
-				port = 8443;
-			} else {
-				port = 8080;
-			}
-		}
-
-		const serverConfig = {
-			port,
-			changePortIfInUse,
-			h2: argv.h2,
-			acceptRemoteConnections: !!argv.acceptRemoteConnections,
-			cert: argv.h2 ? argv.cert : undefined,
-			key: argv.h2 ? argv.key : undefined,
-			sendSAPTargetCSP: !!argv.sapCspPolicies
-		};
-
-		if (!serverConfig.h2) {
-			return {serverConfig, tree};
-		} else {
-			return ui5Server.sslUtil.getSslCertificate(serverConfig.key, serverConfig.cert).then(({key, cert}) => {
-				serverConfig.key = key;
-				serverConfig.cert = cert;
-				return {serverConfig, tree};
-			});
-		}
-	}).then(({serverConfig, tree}) => {
-		return server.serve(tree, serverConfig).then(function({h2, port}) {
-			const protocol = h2 ? "https" : "http";
-			let browserUrl = protocol + "://localhost:" + port;
-			console.log("Server started");
-			console.log("URL: " + browserUrl);
-
-			if (argv.open !== undefined) {
-				if (typeof argv.open === "string") {
-					let relPath = argv.open || "/";
-					if (!relPath.startsWith("/")) {
-						relPath = "/" + relPath;
-					}
-					browserUrl += relPath;
-				}
-				const open = require("open");
-				open(browserUrl);
-			}
-
-			return browserUrl;
-		});
-	}).catch(function(err) {
-		console.error(err);
-		process.exit(1);
 	});
+	let port = argv.port;
+	let changePortIfInUse = false;
+
+	if (!port && tree.server && tree.server.settings) {
+		if (argv.h2) {
+			port = tree.server.settings.httpsPort;
+		} else {
+			port = tree.server.settings.httpPort;
+		}
+	}
+
+	if (!port) {
+		changePortIfInUse = true; // only change if port isn't explicitly set
+		if (argv.h2) {
+			port = 8443;
+		} else {
+			port = 8080;
+		}
+	}
+
+	const serverConfig = {
+		port,
+		changePortIfInUse,
+		h2: argv.h2,
+		acceptRemoteConnections: !!argv.acceptRemoteConnections,
+		cert: argv.h2 ? argv.cert : undefined,
+		key: argv.h2 ? argv.key : undefined,
+		sendSAPTargetCSP: !!argv.sapCspPolicies
+	};
+
+	if (serverConfig.h2) {
+		const {key, cert} = await ui5Server.sslUtil.getSslCertificate(serverConfig.key, serverConfig.cert);
+		serverConfig.key = key;
+		serverConfig.cert = cert;
+	}
+
+	const {h2, port: actualPort} = await server.serve(tree, serverConfig);
+
+	const protocol = h2 ? "https" : "http";
+	let browserUrl = protocol + "://localhost:" + actualPort;
+	console.log("Server started");
+	console.log("URL: " + browserUrl);
+
+	if (argv.open !== undefined) {
+		if (typeof argv.open === "string") {
+			let relPath = argv.open || "/";
+			if (!relPath.startsWith("/")) {
+				relPath = "/" + relPath;
+			}
+			browserUrl += relPath;
+		}
+		const open = require("open");
+		open(browserUrl);
+	}
 };
 
 module.exports = serve;

lib/cli/commands/versions.js
@@ -17,26 +17,21 @@ versions.getVersion = (pkg) => {
 	}
 };
 
-versions.handler = () => {
-	try {
-		const cliVersion = versions.getVersion("../../..");
-		const builderVersion = versions.getVersion("@ui5/builder");
-		const serverVersion = versions.getVersion("@ui5/server");
-		const fsVersion = versions.getVersion("@ui5/fs");
-		const projectVersion = versions.getVersion("@ui5/project");
-		const loggerVersion = versions.getVersion("@ui5/logger");
-		console.log(`
-	@ui5/cli:      ${cliVersion}
-	@ui5/builder:  ${builderVersion}
-	@ui5/server:   ${serverVersion}
-	@ui5/fs:       ${fsVersion}
-	@ui5/project:  ${projectVersion}
-	@ui5/logger:   ${loggerVersion}
-		`);
-	} catch (err) {
-		console.error(err);
-		process.exit(1);
-	}
+versions.handler = async function() {
+	const cliVersion = versions.getVersion("../../..");
+	const builderVersion = versions.getVersion("@ui5/builder");
+	const serverVersion = versions.getVersion("@ui5/server");
+	const fsVersion = versions.getVersion("@ui5/fs");
+	const projectVersion = versions.getVersion("@ui5/project");
+	const loggerVersion = versions.getVersion("@ui5/logger");
+	console.log(`
+@ui5/cli:      ${cliVersion}
+@ui5/builder:  ${builderVersion}
+@ui5/server:   ${serverVersion}
+@ui5/fs:       ${fsVersion}
+@ui5/project:  ${projectVersion}
+@ui5/logger:   ${loggerVersion}
+	`);
 };
 
 module.exports = versions;

test/lib/cli/commands/versions.js
@@ -20,10 +20,9 @@ test.serial("Error: returns not installed if version was not found", (t) => {
 	t.is(versions.getVersion("not/existing/path"), "===(not installed)", "No version found");
 });
 
-test.serial("Error: kills process if error occurred while processing", (t) => {
+test.serial("Error: throws with error if error occurred while processing", async (t) => {
 	sinon.stub(process, "exit");
-	sinon.stub(versions, "getVersion").throws("Error occurred");
-	versions.handler({});
-	t.is(process.exit.getCall(0).args[0], 1, "Process was killed with code 1");
-	process.exit.restore();
+	sinon.stub(versions, "getVersion").throws(new Error("Error occurred"));
+	const error = await t.throwsAsync(versions.handler({}));
+	t.deepEqual(error.message, "Error occurred", "Correct error message thrown");
 });

commit 165fbc28f7ae61127120eadbaaca76d1523396ae

[FIX] Fix 'ui5 tree' output
Refactoring in https://github.com/SAP/ui5-cli/commit/c7c607a6f9337ec80f99b8650bef05fd495a6a71
broke tree logging and logged the yargs command object instead.

Enhanced tests to capture this in the future.

lib/cli/commands/tree.js
@@ -37,13 +37,14 @@ tree.handler = async function(argv) {
 		},
 		configPath: argv.config
 	};
+	let projectTree;
 	if (argv.full) {
-		await normalizer.generateProjectTree(options);
+		projectTree = await normalizer.generateProjectTree(options);
 	} else {
-		await normalizer.generateDependencyTree(options);
+		projectTree = await normalizer.generateDependencyTree(options);
 	}
 
-	const output = argv.json ? JSON.stringify(tree, null, 4) : treeify.asTree(tree, true);
+	const output = argv.json ? JSON.stringify(projectTree, null, 4) : treeify.asTree(projectTree, true);
 	console.log(output);
 };
 

test/lib/cli/commands/tree.js
@@ -27,18 +27,31 @@ test.serial("ui5 tree --full (generates project tree before output)", async (t) 
 
 test.serial("ui5 tree --json (output tree in json)", async (t) => {
 	const jsonStringifySpy = sinon.spy(JSON, "stringify");
+	const consoleStub = sinon.stub(console, "log");
+
 	normalizer.generateDependencyTree.resolves({name: "sample"});
 	await tree.handler({json: true});
-	t.is(JSON.stringify.called, true, "retrieves dependency tree as json");
-	jsonStringifySpy.restore();
+
+	// Note: Some versions of Node.js seem to call stringify internally during this test
+	t.deepEqual(jsonStringifySpy.called, true, "Stringify got called at least once");
+	t.deepEqual(jsonStringifySpy.getCall(jsonStringifySpy.callCount - 1).args[0], {name: "sample"},
+		"JSON.stringify called with correct argument");
+	t.deepEqual(consoleStub.callCount, 1, "console.log was called once");
+	t.deepEqual(consoleStub.getCall(0).args[0], `{
+    "name": "sample"
+}`, "console.log was called with correct argument");
 });
 
 test.serial("ui5 tree (output tree)", async (t) => {
-	const treeifySpy = sinon.spy(treeify, "asTree");
+	const treeifySpy = sinon.stub(treeify, "asTree").returns("🌲");
+	const consoleStub = sinon.stub(console, "log");
 	normalizer.generateDependencyTree.resolves({name: "sample"});
 	await tree.handler({});
-	t.is(treeify.asTree.called, true, "retrieves dependency tree using treeify");
-	treeifySpy.restore();
+
+	t.deepEqual(treeifySpy.callCount, 1, "Treeify called once");
+	t.deepEqual(treeifySpy.getCall(0).args[0], {name: "sample"}, "Treeify called with correct argument");
+	t.deepEqual(consoleStub.callCount, 1, "console.log was called once");
+	t.deepEqual(consoleStub.getCall(0).args[0], "🌲", "console.log was called with correct argument");
 });
 
 test.serial("ui5 tree --dedupe=false (default)", async (t) => {