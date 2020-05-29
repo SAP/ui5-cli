227db8daa780ac13d476e16aab16263cd28b0334
Tobias Sorn <tobias.sorn@sap.com>
2020-05-29 12:44:35 +0200

    [FEATURE] ui5 serve: Add serve-csp-reports option
    Since csp-reports are now served, cli argument is changed.

--- test/lib/cli/commands/serve.js
+++ test/lib/cli/commands/serve.js
@@ -62,7 +62,7 @@ test.serial("ui5 serve: default", async (t) => {
 		cert: undefined,
 		key: undefined,
 		sendSAPTargetCSP: false,
-		cspReportPath: undefined
+		serveCSPReports: false
 	}, "Starting server with specific server config");
 });
 
@@ -100,7 +100,7 @@ test.serial("ui5 serve --h2", async (t) => {
 		key: "randombyte-likes-ponies-key",
 		cert: "randombyte-likes-ponies-cert",
 		sendSAPTargetCSP: false,
-		cspReportPath: undefined
+		serveCSPReports: false
 	}, "Starting server with specific server config");
 });
 
@@ -173,7 +173,7 @@ test.serial("ui5 serve --key --cert", async (t) => {
 		key: "ponies-loaded-from-custompath-key",
 		cert: "ponies-loaded-from-custompath-crt",
 		sendSAPTargetCSP: false,
-		cspReportPath: undefined
+		serveCSPReports: false
 	}, "Starting server with specific server config");
 });
 
@@ -219,7 +219,7 @@ test.serial("ui5 serve --sap-csp-policies", async (t) => {
 		cert: undefined,
 		key: undefined,
 		sendSAPTargetCSP: true,
-		cspReportPath: undefined
+		serveCSPReports: false
 	}, "Starting server with specific server config");
 });
 
@@ -348,13 +348,13 @@ test.serial("ui5 serve: --framework-version", async (t) => {
 	}]);
 });
 
-test.serial("ui5 serve: --csp-report-path=pony", async (t) => {
+test.serial("ui5 serve: --serve-csp-reports=true", async (t) => {
 	normalizerStub.resolves(projectTree);
 	serverStub.resolves({h2: false, port: 8080});
 
 	// loads project tree
 	const pPrepareServerConfig = await serve.handler(
-		Object.assign({}, defaultInitialHandlerArgs, {cspReportPath: "pony"})
+		Object.assign({}, defaultInitialHandlerArgs, {serveCspReports: true})
 	);
 	// preprocess project config, skipping cert load
 	const pServeServer = await pPrepareServerConfig;
@@ -364,5 +364,5 @@ test.serial("ui5 serve: --csp-report-path=pony", async (t) => {
 	const injectedServerConfig = serverStub.getCall(0).args[1];
 
 	t.is(normalizerStub.called, true);
-	t.deepEqual(injectedServerConfig.cspReportPath, "pony", "cspReportPath value set");
+	t.deepEqual(injectedServerConfig.serveCSPReports, true, "serveCSPReports value set");
 });
