3c9e3cbfddb93bcbf348a792138fc1e54e7480cc
Wolfram Woerner <wolfram.woerner@sap.com>
2020-11-05 13:15:10 +0100

    [INTERNAL] Add tests to framework

--- test/lib/framework/create.js
+++ test/lib/framework/create.js
@@ -4,12 +4,12 @@ const mock = require("mock-require");
 const fsHelper = require("../../../lib/utils/fsHelper");
 const fs = require("fs");
 
-async function assertCreateComponent(t, {name, metaInformation, project, expectedMessage, expectedPath,
+async function assertCreate(t, {name, metaInformation, project, expectedMessage, expectedPath,
 	expectedOutput, expectedCallCount}) {
 	const {fsWriteFileStub} = t.context;
-	const createFramework = mock.reRequire("../../../lib/framework/create");
+	const {create} = mock.reRequire("../../../lib/framework/create");
 
-	const {statusMessage} = await createFramework({name, metaInformation, project});
+	const {statusMessage} = await create({name, metaInformation, project});
 
 	t.is(fsWriteFileStub.callCount, expectedCallCount, "Write function should not or once be called");
 	if (expectedCallCount != 0) {
@@ -22,52 +22,20 @@ async function assertCreateComponent(t, {name, metaInformation, project, expecte
 		"statusMessage should be expectedMessage");
 }
 
-async function assertCreateRoute(t, {name, metaInformation, project, expectedMessage, expectedPath,
-	manifest, expectedManifest, expectedCallCount}) {
+async function assertFailingCreate(t, {name, metaInformation, project, expectedMessage, expectedCallCount}) {
 	const {fsWriteFileStub} = t.context;
-	const fsReadFileStub = sinon.stub(fs, "readFile").yieldsAsync(null, manifest);
-
-	const createFramework = mock.reRequire("../../../lib/framework/create");
-
-	const {statusMessage} = await createFramework({name, metaInformation, project});
-
-	t.is(fsWriteFileStub.callCount, expectedCallCount, "Write function should be called");
-	t.deepEqual(fsWriteFileStub.getCall(0).args[0], expectedPath,
-		"Write function should be called with expected path as argument");
-	t.deepEqual(fsWriteFileStub.getCall(0).args[1], expectedManifest,
-		"Write function should be called with expected output as argument");
-	t.is(statusMessage, expectedMessage,
-		"statusMessage should be expectedMessage");
-
-	fsReadFileStub.restore();
-}
-
-async function assertFailingCreateComponent(t, {name, metaInformation, project, expectedMessage, expectedCallCount}) {
-	const {fsWriteFileStub} = t.context;
-	const createFramework = mock.reRequire("../../../lib/framework/create");
-	const exception = await t.throwsAsync(createFramework({name, metaInformation, project}));
+	const {create} = mock.reRequire("../../../lib/framework/create");
+	const exception = await t.throwsAsync(create({name, metaInformation, project}));
 
 	t.is(fsWriteFileStub.callCount, expectedCallCount, "Write function should not be called");
 	t.is(exception.message, expectedMessage, "Create framework should throw expected error");
-}
-
-async function assertFailingCreateRoute(t, {name, metaInformation, project, manifest,
-	expectedMessage, expectedCallCount}) {
-	const {fsWriteFileStub} = t.context;
-	const fsReadFileStub = sinon.stub(fs, "readFile").yieldsAsync(null, manifest);
-	const createFramework = mock.reRequire("../../../lib/framework/create");
-	const exception = await t.throwsAsync(createFramework({name, metaInformation, project}));
-
-	t.is(fsWriteFileStub.callCount, expectedCallCount, "Write function should not be called");
-	t.is(exception.message, expectedMessage, "Create framework should throw expected error");
-
-	fsReadFileStub.restore();
 }
 
 test.beforeEach((t) => {
 	t.context.existsStub = sinon.stub(fsHelper, "exists");
 	t.context.fsMkDirStub = sinon.stub(fs, "mkdir").yieldsAsync(null);
 	t.context.fsWriteFileStub = sinon.stub(fs, "writeFile").yieldsAsync(null);
+
 
 	t.context.consoleLogStub = sinon.stub(console, "log");
 });
@@ -86,15 +54,16 @@ test.serial("Reject process on creating view", async (t) => {
 	};
 
 	existsStub.withArgs("webappPath/view/Test.view.xml").resolves(false);
+	existsStub.withArgs("webappPath").resolves(true);
 	fsMkDirStub.yieldsAsync(new Error("EPERM: operation not permitted, mkdir 'C:\\'"));
 
-	await assertFailingCreateComponent(t, {
+	await assertFailingCreate(t, {
 		name: "test",
 		expectedMessage: "Failed to create component: EPERM: operation not permitted, mkdir 'C:\\'.",
 		metaInformation: {
 			controller: true,
 			type: "view",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
 		expectedCallCount: 0
@@ -132,18 +101,18 @@ test.serial("Reject process on create routing config, already exists", async (t)
 }`;
 
 	existsStub.withArgs("webappPath/view/Test.view.xml").resolves(false);
+	sinon.stub(fs, "readFile").withArgs("webappPath/manifest.json", "utf8").yieldsAsync(null, manifest);
 
-	await assertFailingCreateRoute(t, {
+	await assertFailingCreate(t, {
 		name: "test",
 		expectedMessage: "Route for Test does already exist",
 		metaInformation: {
 			route: true,
 			controller: false,
 			type: "view",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
-		manifest: manifest,
 		expectedCallCount: 0
 	});
 });
@@ -172,18 +141,18 @@ test.serial("Reject process on create routing config, target is missing", async 
 }`;
 
 	existsStub.withArgs("webappPath/view/Test.view.xml").resolves(false);
+	sinon.stub(fs, "readFile").withArgs("webappPath/manifest.json", "utf8").yieldsAsync(null, manifest);
 
-	await assertFailingCreateRoute(t, {
+	await assertFailingCreate(t, {
 		name: "test",
 		expectedMessage: "No valid manifest. 'routes' or 'targets' is missing",
 		metaInformation: {
 			route: true,
 			controller: false,
 			type: "view",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
-		manifest: manifest,
 		expectedCallCount: 0
 	});
 });
@@ -202,18 +171,18 @@ test.serial("Reject process on create routing config, 'sapui5' is missing", asyn
 }`;
 
 	existsStub.withArgs("webappPath/view/Test.view.xml").resolves(false);
+	sinon.stub(fs, "readFile").withArgs("webappPath/manifest.json", "utf8").yieldsAsync(null, manifest);
 
-	await assertFailingCreateRoute(t, {
+	await assertFailingCreate(t, {
 		name: "test",
 		expectedMessage: "No valid manifest. 'sap.ui5' object is missing",
 		metaInformation: {
 			route: true,
 			controller: false,
 			type: "view",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
-		manifest: manifest,
 		expectedCallCount: 0
 	});
 });
@@ -221,7 +190,7 @@ test.serial("Reject process on create routing config, 'sapui5' is missing", asyn
 test.serial("Return message on undefined metaInformation", async (t) => {
 	const project = {};
 
-	await assertCreateComponent(t, {
+	await assertCreate(t, {
 		name: "test",
 		metaInformation: undefined,
 		project: project,
@@ -235,7 +204,7 @@ test.serial("Return message on undefined metaInformation", async (t) => {
 test.serial("Return message on invalid type", async (t) => {
 	const project = {};
 
-	await assertCreateComponent(t, {
+	await assertCreate(t, {
 		name: "test",
 		metaInformation: {
 			type: "xy"
@@ -254,13 +223,13 @@ test.serial("Return message on existing view", async (t) => {
 
 	existsStub.withArgs("webappPath/view/Test.view.xml").resolves(true);
 
-	await assertCreateComponent(t, {
+	await assertCreate(t, {
 		name: "test",
 		expectedMessage: "View Test already exists",
 		metaInformation: {
 			route: false,
 			type: "view",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
 		expectedPath: undefined,
@@ -276,13 +245,13 @@ test.serial("Return message on existing control", async (t) => {
 	existsStub.withArgs("webappPath/control/Test.control.js").resolves(false);
 	existsStub.withArgs("webappPath/control/Test.js").resolves(true);
 
-	await assertCreateComponent(t, {
+	await assertCreate(t, {
 		name: "test",
 		expectedMessage: "Control Test already exists",
 		metaInformation: {
 			route: false,
 			type: "control",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
 		expectedPath: undefined,
@@ -300,21 +269,22 @@ test.serial("Return view message on view created", async (t) => {
 	};
 
 	const output =
-`<mvc:View\r
-    xmlns:mvc="sap.ui.core.mvc">\r
+`<mvc:View
+    xmlns:mvc="sap.ui.core.mvc">
 </mvc:View>`;
 
 	existsStub.withArgs("webappPath/view/Test.view.xml").resolves(false);
+	existsStub.withArgs("webappPath").resolves(true);
 	existsStub.withArgs("webappPath/view").resolves(false);
 
-	await assertCreateComponent(t, {
+	await assertCreate(t, {
 		name: "test",
-		expectedMessage: "view",
+		expectedMessage: "Add new view to project",
 		metaInformation: {
 			route: false,
 			controller: false,
 			type: "view",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
 		expectedPath: "webappPath/view/Test.view.xml",
@@ -332,25 +302,26 @@ test.serial("Return view message on view created with controller and namespace",
 	};
 
 	const output =
-`<mvc:View\r
-    controllerName="xy.controller.Test"\r
-	xmlns:m="sap.m"\r
-    xmlns:mvc="sap.ui.core.mvc">\r
+`<mvc:View
+    controllerName="xy.controller.Test"
+	xmlns:m="sap.m"
+    xmlns:mvc="sap.ui.core.mvc">
 </mvc:View>`;
 
 	existsStub.withArgs("webappPath/view/Test.view.xml").resolves(false);
 	existsStub.withArgs("webappPath/controller/Test.controller.js").resolves(true);
+	existsStub.withArgs("webappPath").resolves(true);
 	existsStub.withArgs("webappPath/view").resolves(true);
 
-	await assertCreateComponent(t, {
+	await assertCreate(t, {
 		name: "test",
-		expectedMessage: "view",
+		expectedMessage: "Add new view with corresponding controller to project",
 		metaInformation: {
 			route: false,
 			controller: true,
 			namespaceList: [{name: "sap.m"}],
 			type: "view",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
 		expectedPath: "webappPath/view/Test.view.xml",
@@ -381,15 +352,16 @@ test.serial("Return controller message on controller created with modules", asyn
 
 	existsStub.withArgs("webappPath/controller/Test.controller.js").resolves(false);
 	existsStub.withArgs("webappPath/controller/Test.js").resolves(false);
+	existsStub.withArgs("webappPath").resolves(true);
 	existsStub.withArgs("webappPath/controller").resolves(true);
 
-	await assertCreateComponent(t, {
+	await assertCreate(t, {
 		name: "test",
-		expectedMessage: "controller",
+		expectedMessage: "Add new controller to project",
 		metaInformation: {
 			moduleList: [{name: "Module1"}, {name: "test/Module2"}],
 			type: "controller",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
 		expectedPath: "webappPath/controller/Test.controller.js",
@@ -407,30 +379,31 @@ test.serial("Return control message on control created", async (t) => {
 	};
 
 	const output =
-`sap.ui.define([\r
-	"sap/ui/core/Control"\r
-], function (Control) {\r
-	"use strict";\r
-	return Control.extend("xy.control.Test", {\r
-		metadata : {\r
-		},\r
-		init : function () {\r
-		},\r
-		renderer : function (oRenderManager, oControl) {\r
-		}\r
-	});\r
+`sap.ui.define([
+	"sap/ui/core/Control"
+], function (Control) {
+	"use strict";
+	return Control.extend("xy.control.Test", {
+		metadata : {
+		},
+		init : function () {
+		},
+		renderer : function (oRenderManager, oControl) {
+		}
+	});
 });`;
 
 	existsStub.withArgs("webappPath/control/Test.control.js").resolves(false);
 	existsStub.withArgs("webappPath/control/Test.js").resolves(false);
+	existsStub.withArgs("webappPath").resolves(true);
 	existsStub.withArgs("webappPath/control").resolves(true);
 
-	await assertCreateComponent(t, {
+	await assertCreate(t, {
 		name: "test",
-		expectedMessage: "control",
+		expectedMessage: "Add new control to project",
 		metaInformation: {
 			type: "control",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
 		expectedPath: "webappPath/control/Test.js",
@@ -472,19 +445,19 @@ test.serial("Return route message on route for existing view created, no routing
 }`;
 
 	existsStub.withArgs("webappPath/view/Test.view.xml").resolves(true);
+	sinon.stub(fs, "readFile").withArgs("webappPath/manifest.json", "utf8").yieldsAsync(null, manifest);
 
-	await assertCreateRoute(t, {
+	await assertCreate(t, {
 		name: "test",
-		expectedMessage: "route",
+		expectedMessage: "Add route to view",
 		metaInformation: {
 			route: true,
 			type: "view",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
-		manifest: manifest,
 		expectedPath: "webappPath/manifest.json",
-		expectedManifest: expectedManifest,
+		expectedOutput: expectedManifest,
 		expectedCallCount: 1
 	});
 });
@@ -523,20 +496,84 @@ test.serial("Return route message on route for existing view created, have routi
     }
 }`;
 
+
+	sinon.stub(fs, "readFile").withArgs("webappPath/manifest.json", "utf8").yieldsAsync(null, manifest);
 	existsStub.withArgs("webappPath/view/Test.view.xml").resolves(true);
 
-	await assertCreateRoute(t, {
+	await assertCreate(t, {
 		name: "test",
-		expectedMessage: "route",
+		expectedMessage: "Add route to view",
 		metaInformation: {
 			route: true,
 			type: "view",
-			webappPath: "webappPath"
+			savePath: "webappPath"
 		},
 		project: project,
-		manifest: manifest,
 		expectedPath: "webappPath/manifest.json",
-		expectedManifest: expectedManifest,
+		expectedOutput: expectedManifest,
+		expectedCallCount: 1
+	});
+});
+
+test.serial("Return bootstrap message on bootstrap created", async (t) => {
+	const {existsStub} = t.context;
+	const project = {
+		metadata: {
+			namespace: "xy"
+		}
+	};
+
+	const manifest =
+`{
+    "sap.app": {
+		"title": "test"
+    }
+}`;
+
+	const output =
+`<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Test</title>
+	<script
+		id="sap-ui-bootstrap"
+		src="resources/sap-ui-core.js"
+		data-sap-ui-theme="sap_fancy_theme"
+		data-sap-ui-resourceroots='{
+			"xy": "./"
+		}'
+		data-sap-ui-oninit="module:sap/ui/core/ComponentSupport"
+		data-sap-ui-compatVersion="edge"
+		data-sap-ui-async="true">
+	</script>
+</head>
+<body class="sapUiBody" id="content">
+	<div data-sap-ui-component data-name="xy" data-id="container" data-settings='{"id" : "xy"}'></div>
+</body>
+</html>`;
+
+	existsStub.withArgs("webappPath/index.html").resolves(false);
+	existsStub.withArgs("webappPath").resolves(true);
+	const fsReadFileStub = sinon.stub(fs, "readFile");
+	fsReadFileStub.withArgs("webappPath/manifest.json", "utf8").yieldsAsync(null, manifest);
+	fs.readFile.callThrough();
+	// .withArgs(__dirname + `/../templates/bootstrap`, "utf8").calls
+
+	await assertCreate(t, {
+		name: undefined,
+		expectedMessage: "Create bootstrap for project",
+		metaInformation: {
+			route: false,
+			controller: false,
+			theme: "sap_fancy_theme",
+			type: "bootstrap",
+			savePath: "webappPath"
+		},
+		project: project,
+		expectedPath: "webappPath/index.html",
+		expectedOutput: output,
 		expectedCallCount: 1
 	});
 });
