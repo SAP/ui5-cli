030ad598307079f6f96a9270e5bc3bc95a8376f5
Matthias Osswald <mat.osswald@sap.com>
2020-11-30 14:31:18 +0100

    [INTERNAL] Update commands/init tests

--- test/lib/cli/commands/init.js
+++ test/lib/cli/commands/init.js
@@ -3,9 +3,14 @@ const sinon = require("sinon");
 const initCommand = require("../../../../lib/cli/commands/init");
 const fsHelper = require("../../../../lib/utils/fsHelper");
 const init = require("../../../../lib/init/init");
-const mockRequire = require("mock-require");
+const mock = require("mock-require");
 const jsYaml = require("js-yaml");
 const fs = require("fs");
+
+test.afterEach.always((t) => {
+	sinon.restore();
+	mock.stopAll();
+});
 
 test.serial("Writes ui5.yaml to fs", async (t) => {
 	const ui5YamlPath = "./ui5.yaml";
@@ -15,32 +20,22 @@ test.serial("Writes ui5.yaml to fs", async (t) => {
 	name: sample-app
 	type: application`;
 
-	mockRequire("path", {resolve: () => ui5YamlPath});
-	const fsHelperStub = sinon.stub(fsHelper, "exists").resolves(false);
-	const initStub = sinon.stub(init, "init").resolves({});
-	const safeDumpYamlStub = sinon.stub(jsYaml, "safeDump").returns(ui5Yaml);
+	mock("path", {resolve: () => ui5YamlPath});
+	sinon.stub(fsHelper, "exists").resolves(false);
+	sinon.stub(init, "init").resolves({});
+	sinon.stub(jsYaml, "safeDump").returns(ui5Yaml);
 	const fsWriteFileStub = sinon.stub(fs, "writeFile").callsArgWith(2);
 
 	await initCommand.handler({});
 
 	t.is(fsWriteFileStub.getCall(0).args[0], ui5YamlPath, "Passes yaml path to write the yaml file to");
 	t.is(fsWriteFileStub.getCall(0).args[1], ui5Yaml, "Passes yaml content to write to fs");
-
-	mockRequire.stop("path");
-	fsWriteFileStub.restore();
-	fsHelperStub.restore();
-	initStub.restore();
-	safeDumpYamlStub.restore();
 });
 
-// FIXME: stubbing process leads to errors, need to be fixed
-// test.serial("Error: throws if ui5.yaml already exists", async (t) => {
-// 	const fileExistsStub = sinon.stub(fsHelper, "exists").resolves(true);
-// 	const processExitStub = sinon.stub(process, "exit");
+test.serial("Error: throws if ui5.yaml already exists", async (t) => {
+	sinon.stub(fsHelper, "exists").resolves(true);
 
-// 	await initCommand.handler();
-// 	t.is(processExitStub.getCall(0).args[0], 1, "killed process on error using process.exit(1)");
-
-// 	fileExistsStub.restore();
-// 	processExitStub.restore();
-// });
+	await t.throwsAsync(initCommand.handler(), {
+		message: "Initialization not possible: ui5.yaml already exists"
+	});
+});
