e3cd22abab65e866755a4a86a40c20d0429af922
Matthias Osswald <mat.osswald@sap.com>
2022-01-24 11:44:34 +0100

    [INTERNAL] Upgrade ESlint / Use ES2021

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
 	"env": {
 		"node": true,
-		"es6": true
-	},
-	"parserOptions": {
-		"ecmaVersion": 8
+		"es2021": true
 	},
 	"extends": ["eslint:recommended", "google"],
 	"plugins": [
@@ -47,7 +44,11 @@ module.exports = {
 		"no-tabs": "off",
 		"no-console": "off", // sometimes needed by CLI
 		"valid-jsdoc": 0,
-		"jsdoc/check-examples": 2,
+		// jsdoc/check-examples is temporarily set to "warn" as the rule causes issues in our CI
+		// See: https://github.com/gajus/eslint-plugin-jsdoc/issues/508
+		// Starting with ESLint v8, it needs to be disabled as it currently can't be supported
+		// See: https://github.com/eslint/eslint/issues/14745
+		"jsdoc/check-examples": 0,
 		"jsdoc/check-param-names": 2,
 		"jsdoc/check-tag-names": 2,
 		"jsdoc/check-types": 2,
