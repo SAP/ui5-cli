1a25fbb60901480da13b47c9705d83def1b7819d
Matthias Osswald <mat.osswald@sap.com>
2022-09-09 12:17:36 +0200

    Remove .then usage in bin/ui5.js test

--- test/bin/ui5.js
+++ test/bin/ui5.js
@@ -1,7 +1,6 @@
 import test from "ava";
 import sinon from "sinon";
 import esmock from "esmock";
-import path from "path";
 import {fileURLToPath} from "url";
 
 test.beforeEach((t) => {
@@ -15,29 +14,13 @@ test.afterEach.always((t) => {
 
 test.serial("ui5 fails when using unsupported Node.js version", async (t) => {
 	const consoleLogStub = sinon.stub(console, "log");
-	const processExitStub = sinon.stub(process, "exit");
 
-	const processExitPromise = new Promise((resolve) => {
+	const processExit = new Promise((resolve) => {
+		const processExitStub = sinon.stub(process, "exit");
 		processExitStub.callsFake((errorCode) => {
 			processExitStub.restore();
 			resolve(errorCode);
 		});
-	}).then((errorCode) => {
-		t.is(errorCode, 1, "Should exit with error code 1");
-		t.is(consoleLogStub.callCount, 6, "console.log should be called 6 times");
-
-		t.deepEqual(consoleLogStub.getCall(0).args,
-			["==================== UNSUPPORTED NODE.JS VERSION ===================="]);
-		t.deepEqual(consoleLogStub.getCall(1).args,
-			["You are using an unsupported version of Node.js"]);
-		t.deepEqual(consoleLogStub.getCall(2).args,
-			[`Detected version ${process.version} but ui5-cli-engines-test requires ^999`]);
-		t.deepEqual(consoleLogStub.getCall(3).args,
-			[""]);
-		t.deepEqual(consoleLogStub.getCall(4).args,
-			["=> Please upgrade to a supported version of Node.js to use this tool"]);
-		t.deepEqual(consoleLogStub.getCall(5).args,
-			["====================================================================="]);
 	});
 
 	await esmock("../../bin/ui5", {
@@ -51,7 +34,23 @@ test.serial("ui5 fails when using unsupported Node.js version", async (t) => {
 			}))
 		}
 	});
-	await processExitPromise;
+	const errorCode = await processExit;
+
+	t.is(errorCode, 1, "Should exit with error code 1");
+	t.is(consoleLogStub.callCount, 6, "console.log should be called 6 times");
+
+	t.deepEqual(consoleLogStub.getCall(0).args,
+		["==================== UNSUPPORTED NODE.JS VERSION ===================="]);
+	t.deepEqual(consoleLogStub.getCall(1).args,
+		["You are using an unsupported version of Node.js"]);
+	t.deepEqual(consoleLogStub.getCall(2).args,
+		[`Detected version ${process.version} but ui5-cli-engines-test requires ^999`]);
+	t.deepEqual(consoleLogStub.getCall(3).args,
+		[""]);
+	t.deepEqual(consoleLogStub.getCall(4).args,
+		["=> Please upgrade to a supported version of Node.js to use this tool"]);
+	t.deepEqual(consoleLogStub.getCall(5).args,
+		["====================================================================="]);
 });
 
 test.serial("ui5 imports local installation when found", async (t) => {
@@ -66,17 +65,6 @@ test.serial("ui5 imports local installation when found", async (t) => {
 			setTimeout(resolve, 0);
 			return true;
 		});
-	}).then(() => {
-		t.is(consoleLogStub.callCount, 0, "console.log should not be called");
-		t.is(consoleInfoStub.callCount, 2, "console.info should be called 2 times");
-
-		t.deepEqual(consoleInfoStub.getCall(0).args, ["INFO: Using local @ui5/cli installation"]);
-		t.deepEqual(consoleInfoStub.getCall(1).args, [""]);
-
-		t.is(importLocalStub.callCount, 1, "import-local should be called once");
-		t.deepEqual(importLocalStub.getCall(0).args, [
-			fileURLToPath(new URL("../../bin/ui5.js", import.meta.url))
-		], "import-local should be called with bin/ui5.js filename");
 	});
 
 	// esmock.p is needed as import-local is loaded via dynamic import
@@ -84,7 +72,18 @@ test.serial("ui5 imports local installation when found", async (t) => {
 		"import-local": {default: importLocalStub}
 	});
 
-	return setTimeoutPromise;
+	await setTimeoutPromise;
+
+	t.is(consoleLogStub.callCount, 0, "console.log should not be called");
+	t.is(consoleInfoStub.callCount, 2, "console.info should be called 2 times");
+
+	t.deepEqual(consoleInfoStub.getCall(0).args, ["INFO: Using local @ui5/cli installation"]);
+	t.deepEqual(consoleInfoStub.getCall(1).args, [""]);
+
+	t.is(importLocalStub.callCount, 1, "import-local should be called once");
+	t.deepEqual(importLocalStub.getCall(0).args, [
+		fileURLToPath(new URL("../../bin/ui5.js", import.meta.url))
+	], "import-local should be called with bin/ui5.js filename");
 });
 
 test.serial("ui5 imports local installation when found (/w --verbose)", async (t) => {
@@ -101,21 +100,6 @@ test.serial("ui5 imports local installation when found (/w --verbose)", async (t
 			setTimeout(resolve, 0);
 			return true;
 		});
-	}).then(() => {
-		t.is(consoleLogStub.callCount, 0, "console.log should not be called");
-		t.is(consoleInfoStub.callCount, 3, "console.info should be called 3 times");
-
-		t.deepEqual(consoleInfoStub.getCall(0).args, [
-			"INFO: This project contains an individual @ui5/cli installation which will be used " +
-			"over the global one."]);
-		t.deepEqual(consoleInfoStub.getCall(1).args, [
-			"See https://github.com/SAP/ui5-cli#local-vs-global-installation for details."]);
-		t.deepEqual(consoleInfoStub.getCall(2).args, [""]);
-
-		t.is(importLocalStub.callCount, 1, "import-local should be called once");
-		t.deepEqual(importLocalStub.getCall(0).args, [
-			fileURLToPath(new URL("../../bin/ui5.js", import.meta.url))
-		], "import-local should be called with bin/ui5.js filename");
 	});
 
 	// esmock.p is needed as import-local is loaded via dynamic import
@@ -123,7 +107,22 @@ test.serial("ui5 imports local installation when found (/w --verbose)", async (t
 		"import-local": {default: importLocalStub}
 	});
 
-	return setTimeoutPromise;
+	await setTimeoutPromise;
+
+	t.is(consoleLogStub.callCount, 0, "console.log should not be called");
+	t.is(consoleInfoStub.callCount, 3, "console.info should be called 3 times");
+
+	t.deepEqual(consoleInfoStub.getCall(0).args, [
+		"INFO: This project contains an individual @ui5/cli installation which will be used " +
+			"over the global one."]);
+	t.deepEqual(consoleInfoStub.getCall(1).args, [
+		"See https://github.com/SAP/ui5-cli#local-vs-global-installation for details."]);
+	t.deepEqual(consoleInfoStub.getCall(2).args, [""]);
+
+	t.is(importLocalStub.callCount, 1, "import-local should be called once");
+	t.deepEqual(importLocalStub.getCall(0).args, [
+		fileURLToPath(new URL("../../bin/ui5.js", import.meta.url))
+	], "import-local should be called with bin/ui5.js filename");
 });
 
 test.serial("ui5 logs warning when using pre-release Node.js version", async (t) => {
@@ -139,32 +138,6 @@ test.serial("ui5 logs warning when using pre-release Node.js version", async (t)
 			setTimeout(resolve, 0);
 			return true;
 		});
-	}).then(() => {
-		t.is(consoleLogStub.callCount, 9, "console.log should be called 8 times");
-
-		t.is(consoleLogStub.getCall(0).args[0],
-			"====================== UNSTABLE NODE.JS VERSION =====================");
-		t.is(consoleLogStub.getCall(1).args[0],
-			"You are using an unstable version of Node.js");
-		t.is(consoleLogStub.getCall(2).args[0],
-			"Detected Node.js version v17.0.0-v8-canary202108258414d1aed8");
-		t.is(consoleLogStub.getCall(3).args[0],
-			"");
-		t.is(consoleLogStub.getCall(4).args[0],
-			"=> Please note that an unstable version might cause unexpected");
-		t.is(consoleLogStub.getCall(5).args[0],
-			"   behavior. For productive use please consider using a stable");
-		t.is(consoleLogStub.getCall(6).args[0],
-			"   version of Node.js! For the release policy of Node.js, see");
-		t.is(consoleLogStub.getCall(7).args[0],
-			"   https://nodejs.org/en/about/releases");
-		t.is(consoleLogStub.getCall(8).args[0],
-			"=====================================================================");
-
-		t.is(importLocalStub.callCount, 1, "import-local should be called once");
-		t.deepEqual(importLocalStub.getCall(0).args, [
-			fileURLToPath(new URL("../../bin/ui5.js", import.meta.url))
-		], "import-local should be called with bin/ui5.js filename");
 	});
 
 	// esmock.p is needed as import-local is loaded via dynamic import
@@ -172,5 +145,31 @@ test.serial("ui5 logs warning when using pre-release Node.js version", async (t)
 		"import-local": {default: importLocalStub}
 	});
 
-	return setTimeoutPromise;
+	await setTimeoutPromise;
+
+	t.is(consoleLogStub.callCount, 9, "console.log should be called 8 times");
+
+	t.is(consoleLogStub.getCall(0).args[0],
+		"====================== UNSTABLE NODE.JS VERSION =====================");
+	t.is(consoleLogStub.getCall(1).args[0],
+		"You are using an unstable version of Node.js");
+	t.is(consoleLogStub.getCall(2).args[0],
+		"Detected Node.js version v17.0.0-v8-canary202108258414d1aed8");
+	t.is(consoleLogStub.getCall(3).args[0],
+		"");
+	t.is(consoleLogStub.getCall(4).args[0],
+		"=> Please note that an unstable version might cause unexpected");
+	t.is(consoleLogStub.getCall(5).args[0],
+		"   behavior. For productive use please consider using a stable");
+	t.is(consoleLogStub.getCall(6).args[0],
+		"   version of Node.js! For the release policy of Node.js, see");
+	t.is(consoleLogStub.getCall(7).args[0],
+		"   https://nodejs.org/en/about/releases");
+	t.is(consoleLogStub.getCall(8).args[0],
+		"=====================================================================");
+
+	t.is(importLocalStub.callCount, 1, "import-local should be called once");
+	t.deepEqual(importLocalStub.getCall(0).args, [
+		fileURLToPath(new URL("../../bin/ui5.js", import.meta.url))
+	], "import-local should be called with bin/ui5.js filename");
 });
