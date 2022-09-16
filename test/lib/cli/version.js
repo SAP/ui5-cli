bcc6c936dd3b21b9c259fb924253220c098c3c32
Matthias Osswald <mat.osswald@sap.com>
2022-09-16 15:04:08 +0200

    Fix tests

--- test/lib/cli/version.js
+++ test/lib/cli/version.js
@@ -5,8 +5,8 @@ test("Set and get version", (t) => {
 	t.is(getVersion(), undefined);
 
 	setVersion("1.2.3");
-	t.is(getVersion("1.2.3"));
+	t.is(getVersion(), "1.2.3");
 
 	setVersion("4.5.6-foo.bar");
-	t.is(getVersion("4.5.6-foo.bar"));
+	t.is(getVersion(), "4.5.6-foo.bar");
 });
