SAP/ui5-cli

commit 5b7cfcf62c37492c4072c69f400512ef59aab502

[FIX] Upgrade update-notifier to solve security vulnerabilities (#533)
Upgrades transitive dependency "got" to resolve CVE-2022-33987.

BCP: 2280109334
BCP: 002075129400006490692022

bin/ui5.js
@@ -38,7 +38,7 @@ if (
 		console.log("=====================================================================");
 	}
 	// Timeout is required to log info when importing from local installation
-	setTimeout(() => {
+	setTimeout(async () => {
 		if (!process.env.UI5_CLI_NO_LOCAL) {
 			const importLocal = require("import-local");
 			// Prefer a local installation of @ui5/cli.
@@ -57,14 +57,26 @@ if (
 			}
 		}
 
-		const updateNotifier = require("update-notifier");
-		updateNotifier({
-			pkg,
-			updateCheckInterval: 1000 * 60 * 60 * 24, // 1 day
-			shouldNotifyInNpmScript: true
-		}).notify();
-		// Remove --no-update-notifier from argv as it's not known to yargs, but we still want to support using it
+		// Only import update-notifier when it's not disabled
+		// See https://github.com/yeoman/update-notifier/blob/3046d0f61a57f8270291b6ab271f8a12df8421a6/update-notifier.js#L57-L60
+		// The "is-ci" check is not executed, but will be checked by update-notifier itself then
 		const NO_UPDATE_NOTIFIER = "--no-update-notifier";
+		const disableUpdateNotifier =
+			"NO_UPDATE_NOTIFIER" in process.env ||
+			process.env.NODE_ENV === "test" ||
+			process.argv.includes(NO_UPDATE_NOTIFIER);
+
+		// Update notifier requires dynamic imports (ES Module) and therefore is loaded via a separate file
+		if (
+			!disableUpdateNotifier &&
+
+			// Node.js versions supporting ES modules
+			semver.satisfies(nodeVersion, "^12.20 || ^14.14 || >= 16.0", {includePrerelease: true})
+		) {
+			const updateNotifier = require("../lib/cli/update-notifier");
+			await updateNotifier({pkg});
+		}
+		// Remove --no-update-notifier from argv as it's not known to yargs, but we still want to support using it
 		if (process.argv.includes(NO_UPDATE_NOTIFIER)) {
 			process.argv = process.argv.filter((v) => v !== NO_UPDATE_NOTIFIER);
 		}

.eslintrc.js
@@ -73,5 +73,15 @@ module.exports = {
 			}
 		}
 	},
-	"root": true
+	"root": true,
+	"overrides": [
+		{
+			"files": [
+				"lib/cli/update-notifier.js"
+			],
+			"parserOptions": {
+				"ecmaVersion": 2020 // minimum version required for dynamic import
+			}
+		}
+	]
 };

lib/cli/update-notifier.js
@@ -1,0 +1,12 @@
+// update-notifier is an ES module and therefore needs to be loaded via "import".
+// It is important to have this code in a separate file as this module also
+// supports Node.js versions without dynamic import support (e.g. v10).
+// Otherwise it will lead to syntax errors.
+module.exports = async function({pkg}) {
+	const updateNotifier = (await import("update-notifier")).default;
+	updateNotifier({
+		pkg,
+		updateCheckInterval: 1000 * 60 * 60 * 24, // 1 day
+		shouldNotifyInNpmScript: true
+	}).notify();
+};

commit b5fe16f59d31a9723f1a4693805456dfd9079a50

[FIX] SyntaxError: Unexpected token '.' in Node.js v12
Regression caused by https://github.com/SAP/ui5-cli/pull/533

Fixes: https://github.com/SAP/ui5-tooling/issues/663

bin/ui5.js
@@ -70,8 +70,9 @@ if (
 		if (
 			!disableUpdateNotifier &&
 
-			// Node.js versions supporting ES modules
-			semver.satisfies(nodeVersion, "^12.20 || ^14.14 || >= 16.0", {includePrerelease: true})
+			// Check for minimum supported Node.js version of update-notifier v6
+			// See https://github.com/yeoman/update-notifier/blob/3046d0f61a57f8270291b6ab271f8a12df8421a6/package.json#L16
+			semver.satisfies(nodeVersion, ">=14.16", {includePrerelease: true})
 		) {
 			const updateNotifier = require("../lib/cli/update-notifier");
 			await updateNotifier({pkg});