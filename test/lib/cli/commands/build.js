b677c954dc2734c0eb2a5feac548c396919eb8a9
Yavor Ivanov <yavor.ivanov@sap.com>
2023-12-06 14:52:02 +0200

    Ignore case for output-style CLI params

--- test/lib/cli/commands/build.js
+++ test/lib/cli/commands/build.js
@@ -25,7 +25,7 @@ function getDefaultArgv() {
 		"experimentalCssVariables": false,
 		"cache-mode": "Default",
 		"cacheMode": "Default",
-		"output-style": "default",
+		"output-style": "Default",
 		"$0": "ui5"
 	};
 }
@@ -370,11 +370,11 @@ test.serial("ui5 build --experimental-css-variables", async (t) => {
 test.serial("ui5 build --output-style", async (t) => {
 	const {build, argv, builder, expectedBuilderArgs} = t.context;
 
-	argv["output-style"] = "flat";
+	argv["output-style"] = "Flat";
 
 	await build.handler(argv);
 
 	expectedBuilderArgs.outputStyle = "Flat";
 	t.deepEqual(builder.getCall(0).args[0], expectedBuilderArgs,
-		"Build with activated outputStyle='flat' is called with expected arguments");
+		"Build with activated outputStyle='Flat' is called with expected arguments");
 });
