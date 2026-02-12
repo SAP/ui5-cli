d1cda145ae1e24f1ffe707e7909dd9046e0f6298
Gianluca Beil <66752411+werwolf2303@users.noreply.github.com>
2026-02-12 15:37:37 +0100

    [INTERNAL] Replace depcheck with knip (#830)
    Due to the deprecation of depcheck we migrated to knip, which is in
    active development and has more features. As a first step we only
    activate the checking of unused dependencies.
    
    The reason why we replaced the docdash jsdoc template path lookup in the
    jsdoc script is that knip fails to detect that a dependency (jsdoc) is
    used when a command inside of the package.json contains JavaScript code
    (via sub-shell).
    
    JIRA: CPOUI5FOUNDATION-1126

---
+++ knip.config.js
@@ -0,0 +1,42 @@
+const config = {
+	/**
+	 * We only need dependency checking at the moment,
+	 * so all checks except for dependencies are turned off.
+	 */
+	rules: {
+		files: "off",
+		duplicates: "off",
+		classMembers: "off",
+		unlisted: "off",
+		binaries: "off",
+		unresolved: "off",
+		catalog: "off",
+		exports: "off",
+		types: "off",
+		enumMembers: "off",
+	},
+
+	ignoreDependencies: [
+		/**
+		 * The package.json files of all @ui5/* packages are dynamically required for the "version" command
+		 * (See lib/cli/commands/versions.js)
+		 * Only @ui5/fs is not used anywhere else in the CLI package, so we need to ignore it here
+		 */
+		"@ui5/fs",
+
+		/**
+		 * Used as jsdoc template in package.json script, which is not detected
+		 */
+		"docdash",
+
+		/**
+		 * Used via nyc ava --node-arguments="--experimental-loader=@istanbuljs/esm-loader-hook"
+		 * which is not detected by knip as a usage of this package
+		 */
+		"@istanbuljs/esm-loader-hook"
+	],
+
+	entry: ["lib/cli/commands/*.js"]
+};
+
+export default config;
