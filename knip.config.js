9a8dc2116d49e7370020bb3e5c3a52e234a4a6df
Gianluca Beil <g.beil@sap.com>
2026-02-12 14:11:26 +0100

    [INTERNAL] Add comments to knip.config.js

--- knip.config.js
+++ knip.config.js
@@ -1,6 +1,7 @@
 const config = {
 	/**
-	 * As we currently only need unused dependency checks, we disable all checks except for that
+	 * We only need dependency checking at the moment,
+	 * so all checks except for dependencies are turned off.
 	 */
 	rules: {
 		files: "off",
@@ -16,8 +17,22 @@ const config = {
 	},
 
 	ignoreDependencies: [
+		/**
+		 * The package.json files of all @ui5/* packages are dynamically required for the "version" command
+		 * (See packages/cli/lib/cli/commands/versions.js)
+		 * Only @ui5/fs is not used anywhere else in the CLI package, so we need to ignore it here
+		 */
 		"@ui5/fs",
+
+		/**
+		 * Used as jsdoc template in package.json script, which is not detected
+		 */
 		"docdash",
+
+		/**
+		 * Used via nyc ava --node-arguments="--experimental-loader=@istanbuljs/esm-loader-hook"
+		 * which is not detected by knip as a usage of this package
+		 */
 		"@istanbuljs/esm-loader-hook"
 	],
 
